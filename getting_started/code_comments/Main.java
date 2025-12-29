fde74d40eb5fbae3c4f71ceb90803f341989def6
GG2802 <>
2025-12-29 18:36:50 +0000

    Code Comments

--- getting_started/code_comments/Main.java
+++ getting_started/code_comments/Main.java
@@ -2,9 +2,10 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey");
-        System.out.println("I'm");    
-        System.out.println("not");
-        System.out.println("working");
+        
+        System.out.println("Hey"); //System.out.println("Hey")   
+        System.out.println("I'm"); //System.out.println("I'm")   
+       // System.out.println("not");  //System.out.println("not")   
+        System.out.println("working"); //System.out.println("working")   
     }
 }
